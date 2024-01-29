14f1761db9fe2bb337a1f5c6b94d924fc40c7775
tahafatmi1 <155869314+tahafatmi1@users.noreply.github.com>
2024-01-29 19:50:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,29 @@ var Name = prompt("Enter your name:");
         document.write('<p style="color: red;">Invalid input. Please enter a valid name.</p>');
     }
 
+
 function isPrime(number) {
-    return "True"
+    if (number <= 1) {
+        return false;
+    }
     
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
 }
- 
+
+var userEnteredNumber = prompt("Enter a positive integer:");
+
+ var number = parseInt(userEnteredNumber);
+
+if (!isNaN(number) && number > 0) {
+    var isPrimeResult = isPrime(number);
+
+    document.write('<p>The number ' + number + ' is prime: ' + isPrimeResult + '</p>');
+} else {
+    document.write('<p style="color: red;">Invalid input. Please enter a valid positive integer.</p>');
+}
